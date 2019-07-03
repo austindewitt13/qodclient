55441eefd8423341abeceb34af20bc8430ee3193
austindewitt13 <50061410+austindewitt13@users.noreply.github.com>
2019-07-03 12:02:45 -0600

    added dialog box and names

--- app/src/main/java/com/example/qod_client/model/Quote.java
+++ app/src/main/java/com/example/qod_client/model/Quote.java
@@ -2,9 +2,14 @@ package com.example.qod_client.model;
 
 import java.net.URI;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 public class Quote {
+
+    private static final String UNKNOWN_SOURCE = "(unknown)";
+    public static final String DELIMITER = ", ";
 
     private UUID id;
 
@@ -13,6 +18,8 @@ public class Quote {
     private Date created;
 
     private URI href;
+
+    private List<Source> sources = new LinkedList<>();
 
     public UUID getId() {
         return id;
@@ -46,4 +53,25 @@ public class Quote {
         this.href = href;
     }
 
+    public List<Source> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<Source> sources) {
+        this.sources = sources;
+    }
+
+    public String getCombinedSources() {
+        StringBuilder builder = new StringBuilder(" \u2014 "); //TODO Extract.
+        if (sources.isEmpty()) {
+            builder.append(UNKNOWN_SOURCE);
+        } else {
+            for (Source source : sources) {
+                builder.append(source.getName());
+                builder.append(DELIMITER);
+            }
+            builder.delete(builder.length() - DELIMITER.length(), builder.length());
+        }
+        return builder.toString();
+    }
 }
